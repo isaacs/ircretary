85918e57261df2c28c4dee5ddcf8ee77247cc298
isaacs <i@izs.me>
2010-12-11 19:33:43 -0800

    redirect to log url

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ http.createServer(function (req, res) {
     res.writeHead(404, {})
     return res.end("404 Not found")
   }
-  res.writeHead(200, {})
-  res.end("Hello")
-}).listen(80)
+  if (process.env.LOGURL) {
+    res.writeHead(301,{location:process.env.LOGURL})
+  } else {
+    res.writeHead(200, {})
+  }
+  res.end("hello")
+}).listen(process.env.PORT || 80)
 
 
 require("./ircretary")
