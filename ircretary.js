97c4e104c96caaf6ecb6bda795f2620be7d76ef9
isaacs <i@izs.me>
2011-09-14 01:17:40 -0700

    join #libuv as well

--- ircretary.js
+++ ircretary.js
@@ -7,7 +7,7 @@ var irc = require("./node_modules/irc")
   , options = { server : "irc.freenode.net"
               , nick : nick
               , channels : ["#node.js", "#isaacs-testing", "#node", "#nodejs"
-                           ,"#joyent"]
+                           ,"#joyent", "#libuv"]
               , userName : "ircretary"
               , realName : "ircretary"
               }
