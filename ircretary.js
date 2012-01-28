04b0cc35085aa3f7e7b6f22d7a6159baa9c9f220
isaacs <i@izs.me>
2012-01-28 14:18:38 -0800

    join stackvm

--- ircretary.js
+++ ircretary.js
@@ -8,7 +8,7 @@ var irc = require("./node_modules/irc")
               , nick : nick
               , channels : ["#node.js", "#isaacs-testing", "#node", "#nodejs"
                            ,"#joyent", "#libuv", "#travis", "#travis-ci"
-                           ,"#nodejitsu"
+                           ,"#nodejitsu", "#stackvm"
                            ,"#thenodefirm"]
               , userName : "ircretary"
               , realName : "ircretary"
